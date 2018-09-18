9f5ffbd1346e7fd6bc13f57e4750706cbc5010c0
Predator12 <panchushak@gmail.com>
2018-09-18 21:15:28 +0300

    Fix bag
    create more content from lab

--- app/src/main/java/com/example/predator/nulpandroid/MainActivity.java
+++ app/src/main/java/com/example/predator/nulpandroid/MainActivity.java
@@ -1,69 +1,27 @@
 package com.example.predator.nulpandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        onClickButtonsHandler();
+
+
     }
 
-    public void onClickButtonsHandler(){
-        final Button helloButton = findViewById(R.id.button);
-        final Button btn_clear = (Button) findViewById(R.id.btn_clear);
-        final EditText nameInput = (EditText) findViewById(R.id.editText);
-        final TextView label = (TextView) findViewById(R.id.textView);
+    public void lab1(View view) {
+        Intent openLaba1 = new Intent(MainActivity.this, Laba_1.class);
+        startActivity(openLaba1);
+    }
 
-        helloButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = nameInput.getText().toString();
-                if (name.matches("")) {
-                    label.setText("Please write your name.");
-                } else {
-                    label.setText("Hello, " + name + "!");
-                }
-                label.setVisibility(View.VISIBLE);
-            }
-        });
-
-        nameInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.toString().trim().length() == 0) {
-                    btn_clear.setVisibility(View.INVISIBLE);
-                } else {
-                    btn_clear.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-
-        btn_clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nameInput.setText("");
-            }
-        });
+    public void lab2(View view) {
+        Intent openLaba2 = new Intent(MainActivity.this, Laba_2.class);
+        startActivity(openLaba2);
     }
 }
